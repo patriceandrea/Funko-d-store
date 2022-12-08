e3e37560a2bb8026838ab1717bfec64491cf12b9
patriceandrea <andrea_pavico98@yahoo.ca>
2022-12-08 12:16:57 -0500

    adds sanity data to client

--- funko'd/pages/index.js
+++ funko'd/pages/index.js
@@ -1,25 +1,38 @@
 import React from 'react'
 import { Product, FooterBanner, HeroBanner } from '../components';
+import { client } from '../lib/client';
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
-
+      <HeroBanner heroBanner={bannerData} />
+      {console.log(bannerData)}
       <div className='products-heading'>
         <h2>Best Selling Product</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div className='products-container'>
-        {['Product 1', ' Product 2'].map((product) => (
-          product
-        ))}
+        {products?.map((product) =>
+          product.name
+        )}
       </div>
 
       <FooterBanner />
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const queryBanner = '*[_type == "banner"]';
+  const bannerData = await client.fetch(queryBanner);
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home;
