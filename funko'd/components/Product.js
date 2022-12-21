e3ee913ebc6e8bf06591b802d522f773c864d107
patriceandrea <andrea_pavico98@yahoo.ca>
2022-12-20 23:19:50 -0500

    adds stripe to pay now button

--- funko'd/components/Product.js
+++ funko'd/components/Product.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Link from 'next/link';
 
 import { urlFor } from '../lib/client';
@@ -7,15 +7,15 @@ const Product = ({ product: { image, name, slug, price } }) => {
   return (
     <div>
       <Link href={`/product/${slug.current}`}>
-        <div className='product-card'>
+        <div className="product-card">
           <img
             src={urlFor(image && image[0])}
-            width={180}
-            height={200}
-            className='product-image'
+            width={250}
+            height={250}
+            className="product-image"
           />
-          <p className='product-name'>{name}</p>
-          <p className='product-price'>${price}</p>
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
         </div>
       </Link>
     </div>
