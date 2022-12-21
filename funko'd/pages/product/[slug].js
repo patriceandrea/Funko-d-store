dd9f7f0680e81b1181d86f96e2327206203ec516
patriceandrea <andrea_pavico98@yahoo.ca>
2022-12-21 14:38:11 -0500

    adds buy now functionality

--- funko'd/pages/product/[slug].js
+++ funko'd/pages/product/[slug].js
@@ -9,8 +9,12 @@ import { Toaster } from 'react-hot-toast';
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0)
-  const { decQty, incQty, qty, onAdd } = useStateContext();
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+    setShowCart(true);
+  };
 
   return (
     <div>
@@ -80,6 +84,7 @@ const ProductDetails = ({ product, products }) => {
             <button
               type='button'
               className='buy-now'
+              onClick={handleBuyNow}
             >
               Buy now
             </button>
