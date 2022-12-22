db49c9613038e136d33cd3b5278a10bbf102d67b
patriceandrea <andrea_pavico98@yahoo.ca>
2022-12-21 21:39:23 -0500

    modify eslintrc

--- funko'd/pages/_app.js
+++ funko'd/pages/_app.js
@@ -1,18 +1,17 @@
-import '../styles/globals.css'
-import React from 'react'
+import '../styles/globals.css';
+import React from 'react';
 import { Layout } from '../components';
 import { StateContext } from '../context/StateContext';
 
 
-function MyApp({ Component, pageProps }) {
+const MyApp = ({ Component, pageProps }) => {
   return (
     <StateContext>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </StateContext>
-  )
-}
+  );
+};
 
-
-export default MyApp
+export default MyApp; 
